bc25a6015c3b60e3c9b0925942d4e4c991b499c8
Axel Prevoteaux <axel.prevoteaux@gmail.com>
2019-08-07 16:49:16 +0200

    made a bit a style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Le Dojo Nantais',
+  address: '26 boulevard Stalingrad',
+  description: 'A nice place for work',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'O bistro quai',
+  address: 'Nantes',
+  description: 'A nice something to do some stuff',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Le Warehouse',
+  address: 'Nantes',
+  description: 'Place for party',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+puts "Flats made!"
